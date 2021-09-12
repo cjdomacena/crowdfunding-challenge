3148767c192d8909dee506e68a451953f7532184
poseidon10 <cjdomacena@yahoo.com>
2021-09-11 22:53:00 -0400

    Added Navigation Bar - not yet responsive

--- src/Components/Navbar/Navbar.style.js
+++ src/Components/Navbar/Navbar.style.js
@@ -1,3 +1,32 @@
 import styled from 'styled-components'
 
 
+export const Navbar = styled.nav`
+display:flex;
+justify-content: space-between;
+align-items:center;
+padding: 2rem;
+position: absolute;
+top: 0;
+left:0;
+right:0;
+font-family: ${$props => $props.theme.font.primary};
+max-width: ${$props=>$props.theme.viewport.desktop};
+margin: 0 auto;
+`
+
+export const NavBrand = styled.div``
+
+export const NavItems = styled.ul`
+display:flex;
+background-color:white;
+`
+
+export const NavLink = styled.li`
+padding: 0 2rem;
+cursor:pointer;
+&:hover
+{
+    color:red;
+}
+`
---
+++ src/Components/Theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+
+        font: {
+            primary: "'Commissioner', sans-serif;",
+            fontWeight: {
+                "normal": 400,
+                "medium": 500,
+                "bold": 700
+            }
+        },
+        colors: {
+           primary: {
+               moderateCyan: 'hsl(176, 50%, 47%)',
+               darkCyan: 'hsl(176, 72%, 28%)'
+           },
+           nuetral: {
+               black: 'hsl(0, 0%, 0%)',
+               darkGray: 'hsl(0, 0%, 48%)'
+           }
+        },
+        viewport: {
+            desktop: '1440px',
+            mobile: '375px'
+        }
+}
