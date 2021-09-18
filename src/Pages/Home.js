451d33a2f2aba904a71c037a0fd68e20c39205c3
cjdomacena <cjdomacena@yahoo.com>
2021-09-18 15:05:14 -0400

    Started Fund Component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './../Components/Navbar'
 import './../App.css'
-import Bookmark from '../Components/Bookmark'
+import  FundContainer from '../Components/FundTracker/FundContainer/'
 const Home = () => {
     return (
         <>
             <Navbar/>
             <div className="hero-background"></div>
-            <Bookmark/>
+            <FundContainer></FundContainer>
         </>
     )
 }
