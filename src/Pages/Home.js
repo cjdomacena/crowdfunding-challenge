a8145755a8a11ca29d56fe2cef26fba87ab23ccb
poseidon10 <cjdomacena@yahoo.com>
2021-09-18 20:12:06 -0400

    Fixed merge issue

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import Navbar from './../Components/Navbar'
 import './../App.css'
-<<<<<<< HEAD
-=======
 import  FundContainer from '../Components/FundTracker/FundContainer/'
->>>>>>> 451d33a2f2aba904a71c037a0fd68e20c39205c3
 const Home = () => {
     return (
         <>
             <Navbar/>
             <div className="hero-background"></div>
-<<<<<<< HEAD
-=======
             <FundContainer></FundContainer>
->>>>>>> 451d33a2f2aba904a71c037a0fd68e20c39205c3
         </>
     )
 }
